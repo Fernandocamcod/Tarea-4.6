3ccf42ce63e79f045314a29a3876b578cff6f969
Fernandocamcod <williamfernando1981@gmail.com>
2024-08-12 19:40:23 -0500

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Definimos un arreglo de números
+        int[] numeros = { 10, 20, 30, 40, 50, 60, 70, 80, 90, 100 };
+
+        Console.WriteLine("Ingrese el número que desea buscar:");
+        int elementoBuscado = int.Parse(Console.ReadLine());
+
+        // Variable para indicar si se encontró el elemento
+        bool encontrado = false;
+
+        // Bucle for para buscar el elemento en el arreglo
+        for (int i = 0; i < numeros.Length; i++)
+        {
+            if (numeros[i] == elementoBuscado)
+            {
+                encontrado = true; // Se encontró el elemento
+                break; // Salir del bucle
+            }
+        }
+
+        // Mostrar el resultado
+        if (encontrado)
+        {
+            Console.WriteLine($"El número {elementoBuscado} fue encontrado en el arreglo.");
+        }
+        else
+        {
+            Console.WriteLine($"El número {elementoBuscado} no fue encontrado en el arreglo.");
+        }
+
+        // Esperar a que el usuario presione una tecla
+        Console.WriteLine("Presiona cualquier tecla para continuar...");
+        Console.ReadKey();
+    }
+}
